2ee6b07970676d29d4e192db47d9ac96ac40c1e3
Rychard <rychardanthony33@gmail.com>
2025-06-26 17:15:21 -0300

    Adição de filtro

--- app/controllers/user/explorar_controller.rb
+++ app/controllers/user/explorar_controller.rb
@@ -1,11 +1,13 @@
 class User::ExplorarController < ApplicationController
   layout "user_dashboard"
 
+
   def index
+    
     @all_cursos = Curso.all
 
-    if params[:query].present?
-      @all_cursos = Curso.find_by_name(params[:query])
+    if params[:order_by].present?
+      @all_cursos = Curso.find_by_order(params[:order_by])
     end
   end
 end
